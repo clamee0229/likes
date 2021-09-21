c82851d8e60223151e1de5101da17fcf4e3cbc93
clamee0229 <chris.lamee96@gmail.com>
2021-09-21 14:34:45 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var likes1 = document.querySelector("#likes1");
+var counter1 = 9;
+
+function like1(){
+    counter1++;
+    likes1.innerText = counter1 +" Like(s)";
+}
+
+var likes2 = document.querySelector("#likes2");
+var counter2 = 12;
+
+function like2(){
+    counter2++;
+    likes2.innerText = counter2 +" Like(s)";
+}
+
+var likes3 = document.querySelector("#likes3");
+var counter3 = 9;
+
+function like3(){
+    counter3++;
+    likes3.innerText = counter3 +" Like(s)";
+}
